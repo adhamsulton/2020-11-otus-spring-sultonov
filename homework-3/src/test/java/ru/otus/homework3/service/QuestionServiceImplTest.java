936a760004f00468705ee99429b514a1940d9882
Adham <adhamsultonov@gmail.com>
2020-12-17 16:17:51 +0500

    fixed

--- homework-3/src/test/java/ru/otus/homework3/service/QuestionServiceImplTest.java
+++ homework-3/src/test/java/ru/otus/homework3/service/QuestionServiceImplTest.java
@@ -4,15 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import ru.otus.homework3.config.AppProps;
-import ru.otus.homework3.dao.QuestionDao;
 import ru.otus.homework3.domain.User;
-
-import java.util.Scanner;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,20 +13,12 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @SpringBootTest
 class QuestionServiceImplTest {
-    @Autowired
+    @MockBean
     private ReadWriteService readWriteService;
     @MockBean
     private UserService userService;
     @Autowired
     private QuestionService questionService;
-
-//    @TestConfiguration
-//    class NestedConfiguration {
-//        @Bean
-//        ReadWriteService readWriteService() {
-//            return new ReadWriteServiceImpl(System.out, new Scanner(System.in));
-//        }
-//    }
 
     @BeforeEach
     public void setUp() {
@@ -46,6 +31,6 @@ class QuestionServiceImplTest {
     void runTest() {
         questionService.runTest();
         verify(userService, times(1)).getUser();
-        verify(readWriteService, times(1)).print("Welcome to testing " + "Ivan");
+        verify(readWriteService, times(1)).print("Welcome to testing " + "Ivan!");
     }
 }
