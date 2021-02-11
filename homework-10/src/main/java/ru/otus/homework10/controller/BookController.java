ba758cacdae56af2acb1cabb62ce59cc16dabb32
adhamsulton <adhamsulton@gmail.com>
2021-02-11 09:54:27 +0500

    review

--- homework-10/src/main/java/ru/otus/homework10/controller/BookController.java
+++ homework-10/src/main/java/ru/otus/homework10/controller/BookController.java
@@ -2,53 +2,34 @@ package ru.otus.homework10.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.otus.homework10.domain.Author;
 import ru.otus.homework10.domain.Book;
-import ru.otus.homework10.domain.Genre;
 import ru.otus.homework10.dto.BookDto;
-import ru.otus.homework10.service.AuthorService;
 import ru.otus.homework10.service.BookService;
-import ru.otus.homework10.service.CommentService;
-import ru.otus.homework10.service.GenreService;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
-    private final GenreService genreService;
-    private final AuthorService authorService;
-    private final CommentService commentService;
 
-    @GetMapping
+    @GetMapping("/books")
     public List<Book> fetch() {
         return bookService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/books/{id}")
     public Book get(@PathVariable("id") Long id) {
         return bookService.findById(id).orElse(new Book());
     }
 
-    @PostMapping("/save")
+    @PostMapping("/books")
     public Book save(@RequestBody BookDto bookDto) {
         return bookService.save(bookDto);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/books/{id}")
     public void delete(@PathVariable("id") Long id) {
         bookService.delete(id);
-    }
-
-    @GetMapping("/genres")
-    public List<Genre> fetchGenres() {
-        return genreService.findAll();
-    }
-
-    @GetMapping("/authors")
-    public List<Author> fetchAuthors() {
-        return authorService.findAll();
     }
 }
