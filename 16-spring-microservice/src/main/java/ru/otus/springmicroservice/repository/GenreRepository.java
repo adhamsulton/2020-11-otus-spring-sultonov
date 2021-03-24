78c66baccbc53474c5c05bd38d4f4569c8a40218
adhamsulton <adhamsulton@gmail.com>
2021-03-25 00:11:00 +0500

    16

---
+++ 16-spring-microservice/src/main/java/ru/otus/springmicroservice/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package ru.otus.springmicroservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.otus.springmicroservice.domain.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
