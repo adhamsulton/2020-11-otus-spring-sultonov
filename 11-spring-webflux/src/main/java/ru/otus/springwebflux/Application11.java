bad9eb6c564db927954a4fba96f0809404759483
adhamsulton <adhamsulton@gmail.com>
2021-09-15 12:24:06 +0500

    reactive_mongock

--- 11-spring-webflux/src/main/java/ru/otus/springwebflux/Application11.java
+++ 11-spring-webflux/src/main/java/ru/otus/springwebflux/Application11.java
@@ -1,11 +1,11 @@
 package ru.otus.springwebflux;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("ru.otus.springwebflux.controller")
+@EnableMongock
 public class Application11 {
 
     public static void main(String[] args) {
